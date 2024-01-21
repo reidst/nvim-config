cb270f2747a1fa14b3b811eb4d4164f334080fb1
Katherine <reidst@hendrix.edu>
2024-01-21 13:18:27 -0600

    typo in lazy.nvim bootstrap

--- init.lua
+++ init.lua
@@ -94,7 +94,7 @@ if not vim.loop.fs_stat(lazypath) then
         "git",
         "clone",
         "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim/git",
+        "https://github.com/folke/lazy.nvim.git",
         "--branch=stable",
         lazypath,
     })
